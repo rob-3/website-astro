6e078fbfebf8fe80a879f433ea979ac0d719e1d5
Robert Boyd III <git@robertboyd.dev>
2023-02-11 18:36:56 -0500

    Add resume support

---
+++ src/pages/resume.pdf.ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro";
+
+export const get: APIRoute = async () => {
+	const resumeResponse = await fetch("https://api.github.com/repos/rob-3/resume/contents/resume.pdf", {
+		headers: {
+			Accept: "application/vnd.github.raw",
+			Authorization: `Bearer ${import.meta.env.GITHUB_API_TOKEN}`,
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	});
+	const resumePdf = Buffer.from(await resumeResponse.arrayBuffer()).toString("binary");
+	return {
+		body: resumePdf,
+		encoding: "binary",
+	}
+}
