6b5459b93d7f4ce544b23d732bdcb14a4a925da4
Robert Boyd III <git@robertboyd.dev>
2023-04-02 14:37:30 -0400

    Try to fix RepoCard in mobile chrome

--- src/components/RepoCard.svelte
+++ src/components/RepoCard.svelte
@@ -20,7 +20,7 @@
 				d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
 		/></svg
 		>
-		<span style="font-weight: 600; color: #0366d6;">
+		<span style="font-weight: 600; color: #0366d6;" class="w-full">
 			<a style="color: inherit;" href={html_url}>{name}</a>
 		</span>
 	</div>
@@ -37,12 +37,11 @@
 		>
 		{description || 'No description, website, or topics provided.'}
 	</div>
-	<div style="font-size: 12px; color: #586069; display: flex;">
-		<div style="{language ? '' : 'display: none'}; margin-right: 16px;">
+	<div style="font-size: 12px; color: #586069;" class="flex gap-2">
+		<div style="{language ? '' : 'display: none'};" class="flex items-center">
 			<span
-				style="width: 12px; height: 12px; border-radius: 100%; background-color: {language
-				? colors[language].color
-				: ''}; display: inline-block; top: 1px; position: relative;"
+				class="w-3 h-3 rounded-full mr-[6px]"
+				style={`background-color: ${language ? colors[language].color : ''};`}
 			/>
 			<span>{language}</span>
 		</div>
