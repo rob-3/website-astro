f6e8ebd4b058bfb325ccbec196c1adcbebe57c42
Robert Boyd III <git@robertboyd.dev>
2023-06-22 23:17:56 -0700

    Refactor card

--- src/components/RepoCard.svelte
+++ src/components/RepoCard.svelte
@@ -15,36 +15,14 @@
 	} = repo);
 </script>
 
-<div in:fade class="card">
-	<div class="center">
-		<svg
-			style="fill: #586069; margin-right: 8px;"
-			viewBox="0 0 16 16"
-			version="1.1"
-			width="16"
-			height="16"
-			aria-hidden="true"
-			><path
-				fill-rule="evenodd"
-				d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
-			/></svg
-		>
-		<span style="font-weight: 600; color: #0366d6;" class="w-full">
-			<a style="color: inherit;" href={html_url}>{name}</a>
-		</span>
-	</div>
+<div in:fade>
+	<a href={html_url}>{name}</a>
 	{#if fork}
-		<div class="fork">
-			Forked from <a
-				style="color: inherit; text-decoration: none;"
-				href={source.html_url}>{source.full_name}</a
-			>
+		<div class="text-xs text-[#586069]">
+			Forked from <a href={source.html_url}>{source.full_name}</a>
 		</div>
 	{/if}
-	<div
-		class="description"
-		style="font-size: 12px; margin-bottom: 8px; margin-top: 8px; color: #586069;"
-	>
+	<div class="my-2 text-xs text-[#586069]">
 		{description || "No description, website, or topics provided."}
 	</div>
 	<div
@@ -102,37 +80,6 @@
 </div>
 
 <style>
-	.card {
-		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica,
-			Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
-		border: 1px solid #e1e4e8;
-		border-radius: 0.375em;
-		background: white;
-		padding: 1.2em;
-		line-height: 1.5;
-		color: #24292e;
-		min-height: 8.2em;
-		height: 100%;
-		box-sizing: border-box;
-		box-shadow: 0 0.25em 0.5em 0 rgba(0, 0, 0, 0.2),
-			0 0.375em 1.25em 0 rgba(0, 0, 0, 0.19);
-		display: flex;
-		flex-direction: column;
-	}
-	.center {
-		display: flex;
-		align-items: center;
-	}
-	.fork {
-		font-size: 0.75em;
-		color: #586069;
-	}
-	.description {
-		flex-grow: 1;
-	}
-	a {
-		text-decoration: none;
-	}
 	a:hover {
 		text-decoration: underline;
 	}
