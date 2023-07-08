b94adfac475b0d829141d8bf86f2f1e5fb631f0c
Robert Boyd III <git@robertboyd.dev>
2023-07-08 10:26:41 -0700

    Add RSS support

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export async function get(context) {
 	const posts = await getCollection('blog');
@@ -9,8 +13,9 @@ export async function get(context) {
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
 			link: `/blog/${post.slug}/`,
+			content: sanitizeHtml(parser.render(post.body)),
+			...post.data,
 		})),
 	});
 }
