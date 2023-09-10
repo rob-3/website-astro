5a4c68fcbea489e03057011ae6567c8d53301598
Robert Boyd III <git@robertboyd.dev>
2023-09-10 16:40:14 -0400

    Delete some old dead description code

--- src/components/GitHubPortfolio.svelte
+++ src/components/GitHubPortfolio.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import RepoCard from "../components/RepoCard.svelte";
   import { flip } from "svelte/animate";
-  import { fade } from "svelte/transition";
   import type { GitHubRepositoryData } from "../components/GitHubRepositoryData";
 
   export let repos: GitHubRepositoryData[] = [];
@@ -15,9 +14,6 @@
     </div>
   {/each}
 </div>
-{#if myRepos.length === 1}
-  <div in:fade class="description">{@html myRepos[0].description}</div>
-{/if}
 
 <style>
   .card {
@@ -31,16 +27,5 @@
     justify-content: center;
     gap: 20px;
     font-size: 1rem;
-  }
-  .description {
-    width: min(25em, 100%);
-    background-color: white;
-    border: 1px solid #e1e4e8;
-    box-sizing: border-box;
-    box-shadow:
-      0 0.25em 0.5em 0 rgba(0, 0, 0, 0.2),
-      0 0.375em 1.25em 0 rgba(0, 0, 0, 0.19);
-    border-radius: 0.375em;
-    padding: 1.2em;
   }
 </style>
